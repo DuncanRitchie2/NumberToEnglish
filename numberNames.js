ebab79090f0b73b10b41339c8c260894b3c043d4
Duncan Ritchie <duncanritchie@btopenworld.com>
2019-09-09 17:41:40 +0100

    Added decimals (with floating-point bugs)

--- numberNames.js
+++ numberNames.js
@@ -1,45 +1,61 @@
 // Converts integers between 0 and 19,999 to English,
 // e.g. 19,992 -> "nineteen thousand, nine hundred and ninety-two"
 const numToEng = (n) => {
-    if (n===0) return "zero"; 
-	const smalls = ["one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
-	const tens = ["ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
-	let outputWords = [];
-	if (n>=1000) {
-		outputWords.push(smalls[Math.floor(n/1000)-1]);
-		outputWords.push("thousand");
-		n=n%1000;
-        if (n) {
-            if (n>=100) {
-                outputWords[outputWords.length-1]+=",";
+    let outputWords = [];
+        const smalls = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+    // Save any decimal part for later.
+    let decimal = n-Math.floor(n);
+    n = Math.floor(n)
+    // If zero...
+    if (n===0) {outputWords.push("zero");}
+    else {
+        // If non-zero...
+        const tens = ["zero","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
+        if (n>=1000) {
+            outputWords.push(smalls[Math.floor(n/1000)]);
+            outputWords.push("thousand");
+            n=n%1000;
+            if (n) {
+                if (n>=100) {
+                    outputWords[outputWords.length-1]+=",";
+                }
+                else {
+                    outputWords.push("and");
+                }
             }
-            else {
+        }
+        if (n>=100) {
+            outputWords.push(smalls[Math.floor(n/100)]);
+            outputWords.push("hundred");
+            n=n%100;
+            if (n) {
                 outputWords.push("and");
             }
         }
-	}
-	if (n>=100) {
-		outputWords.push(smalls[Math.floor(n/100)-1]);
-		outputWords.push("hundred");
-        n=n%100;
-        if (n) {
-            outputWords.push("and");
+        if (n>=20) {
+            outputWords.push(tens[Math.floor(n/10)]);
+            n-=Math.floor(n/10)*10;
+            if (n%10) {
+                outputWords[outputWords.length-1]+="-"+smalls[n-1];
+                n-=smalls[n-1];
+            }
         }
-	}
-	if (n>=20) {
-		outputWords.push(tens[Math.floor(n/10)-1]);
-        n-=Math.floor(n/10)*10;
-        if (n%10) {
-            outputWords[outputWords.length-1]+="-"+smalls[n-1];
-            n-=smalls[n-1];
+        if (n) {
+            outputWords.push(smalls[n]);
         }
     }
-    if (n) {
-        outputWords.push(smalls[n-1]);
+    
+    if (decimal) {
+        outputWords.push("point");
+        while (decimal) {
+            decimal*=10
+            outputWords.push(smalls[Math.floor(decimal)])
+            decimal-=Math.floor(decimal);
+        }
     }
 	return outputWords.join(" ");
 }
 
-for(let i = 19990; i < 19993; i++) {
-    console.log(numToEng(i));
+for(let i = 0; i <= 300; i++) {
+    console.log(numToEng(i/100));
 }
