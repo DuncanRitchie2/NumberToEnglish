5d79edd5a6ae5b6270d8e3a4a0031a11e75e8505
Duncan Ritchie <duncanritchie@btopenworld.com>
2019-09-09 16:54:27 +0100

    Second commit

--- numberNames.js
+++ numberNames.js
@@ -1,17 +1,28 @@
-function numToEng(n) {
-	if (n===0) return "zero"; 
-	const smallNames = ["one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
-	const tenNames = ["ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
+// Converts integers between 0 and 19,999 to English,
+// e.g. 19,992 -> "nineteen thousand nine hundred ninety two"
+const numToEng = (n) => {
+    if (n===0) return "zero"; 
+	const smalls = ["one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+	const tens = ["ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
 	let outputWords = [];
+	if (n>=1000) {
+		outputWords.push(smalls[Math.floor(n/1000)-1]);
+		outputWords.push("thousand");
+		n=n%1000;
+	}
 	if (n>=100) {
-		outputWords.push(smallNames[Math.floor(n/100)-1]);
+		outputWords.push(smalls[Math.floor(n/100)-1]);
 		outputWords.push("hundred");
 		n=n%100;
 	}
 	if (n>=20) {
-		outputWords.push(tenNames[Math.floor(n/10)-1]);
+		outputWords.push(tens[Math.floor(n/10)-1]);
 		n-=Math.floor(n/10)*10;
 	}
-	outputWords.push(smallNames[n-1]);
+	outputWords.push(smalls[n-1]);
 	return outputWords.join(" ");
+}
+
+for(let i = 0; i < 20000; i++) {
+    console.log(numToEng(i));
 }
